4019cb0f9d894b19f47c34a01bad972313868a67
Walescko <walescko@gmail.com>
2022-06-04 07:15:07 -0300

    c - adjust

--- Tema02/Enter002modificate.c
+++ Tema02/Enter002modificate.c
@@ -1,3 +1,17 @@
+#include <stdio.h>
 //
 // Created by walescko on 04/06/22.
 //
+void main(){
+
+    float dividendo
+    int divisor;
+
+    printf("Entre com o valor do dividendo e disivor respectivamente:\n");
+    scanf("%f %d", &dividendo, &divisor);
+
+    printf("\nA divisão de %2f por %d é %.2f", dividendo, divisor, dividendo/divisor);
+
+    printf("\nThe End");
+
+}
