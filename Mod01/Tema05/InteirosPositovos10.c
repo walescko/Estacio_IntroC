028b060eda453957c11649d0de9399bf7ced20c8
Walescko <walescko@gmail.com>
2022-06-05 22:54:29 -0300

    C - inteiros de 1 a 10

--- Mod01/Tema05/InteirosPositovos10.c
+++ Mod01/Tema05/InteirosPositovos10.c
@@ -1,3 +1,15 @@
-//
+#include <stdio.h>//
 // Created by walescko on 05/06/22.
 //
+void main (){
+
+    int number;
+
+    printf("\nOs 10 primeiros numeros inteiros positivos em ordem crescente\n");
+
+    for(int i = 0; i < 10; i++){
+    printf("%d ", i+1);
+    }
+    printf("\n");
+
+}
