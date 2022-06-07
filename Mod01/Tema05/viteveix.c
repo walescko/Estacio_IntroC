7f1f9fd02660acb875c4066684c2fb8054a891d3
Walescko <walescko@gmail.com>
2022-06-07 05:38:16 -0300

    C - for 20

---
+++ Mod01/Tema05/viteveix.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+//
+// Created by walescko on 06/06/22.
+//
+int main(void){
+
+    int number01;
+    printf("\nEntre com um valor a ser repetido: ");
+    scanf("%d",&number01);
+
+    for(int i = 0 ; i<20; i++){
+        printf("%d ", number01);
+    }
+
+    printf("\nEND\n");
+
+
+
+}
