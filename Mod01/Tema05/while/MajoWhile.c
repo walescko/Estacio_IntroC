c1e6b86480f4b277d8f1e9ea89c0ad87dc375d1e
Walescko <walescko@gmail.com>
2022-06-12 12:23:53 -0300

    c - while 2

--- Mod01/Tema05/while/MajoWhile.c
+++ Mod01/Tema05/while/MajoWhile.c
@@ -4,17 +4,22 @@
 //
 int main(void){
 
-    int numero, maior=0, i=5;
+    int numero, maior=0;
+    int i = 0; //variavel de controle do while
 
     printf("\nEntre com um número: ");
-    scanf("%d", numero);
+    scanf("%d", &numero);
 
-    while(i <= 5){
+    while(i < 4){//cheiro de gambiarra
         if (maior < numero){
             maior = numero;
         }
         printf("\nEntre com um número: ");
         scanf("%d", &numero);
+        i++;
     }
-    printf("\n")
+    printf("\nO maior número digitado foi %d", maior);
+    printf("\nEnd\n");
+    
+    return 0;
 }
