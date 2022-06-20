8f20be0b7486a2a7a83f62e30153d5aef9373706
Walescko <walescko@gmail.com>
2022-06-20 17:50:21 -0300

    c - array average

---
+++ Mod01/Tema06/vectors/AverageClass.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+//
+// Created by walescko on 20/06/22.
+//
+//Faça um programa que leia a nota de 20 alunos da turma
+// e mostre as que são iguais ou superiores à média da turma.
+
+int main(void){
+
+    double notas[20], average = 0, sum = 0;
+
+    return 0;
+}
