a6fabf2b472b2e6ce6efab9ba077577b09777ac0
Walescko <walescko@gmail.com>
2022-06-21 06:24:50 -0300

    c - adjust

--- Mod01/Tema06/vectors/AverageClass.c
+++ Mod01/Tema06/vectors/AverageClass.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 //
 // Created by walescko on 20/06/22.
 //
@@ -8,6 +9,23 @@
 int main(void){
 
     double notas[20], average = 0, sum = 0;
+
+    for(int i = 0; i<20; i++){
+        notas[i] = rand() % 10;
+        sum = sum + notas[i];
+        printf("\nNota do %dº aluno: %.2lf", i+1, notas[i]);
+    }
+
+    average = sum / 20;
+    printf("\nMédia da turma: %.2lf\n", average);
+
+    for(int i=0; i<20; i++){
+        if(notas[i] >= average){
+            printf("O aluno %d está acima da média. Nota %.2lf.\n", i+1, notas[i]);
+        } else {
+            printf("O aluno %d está abaixo da média. Nota %.2lf.\n", i+1, notas[i]);
+        }
+    }
 
     return 0;
 }
