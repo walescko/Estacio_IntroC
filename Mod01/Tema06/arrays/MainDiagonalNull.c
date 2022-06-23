587b6e63bd4f56f17576804c1e795f9c3438282a
Walescko <walescko@gmail.com>
2022-06-23 20:17:15 -0300

    c

--- Mod01/Tema06/arrays/MainDiagonalNull.c
+++ Mod01/Tema06/arrays/MainDiagonalNull.c
@@ -1,3 +1,24 @@
+#include <stdio.h>
 //
 // Created by walescko on 23/06/22.
 //
+
+int main(void){
+
+    int n = 5, m = 5;
+    int unique[n][m];
+
+    for(int i = 0; i < n; i++) {
+        printf("\n");
+        for(int j = 0; j<m; j++){
+            if(i==j){
+                unique[i][j]=0;
+            } else {
+                unique[i][j]=1;
+            }
+            printf("%d ", unique[i][j]);
+        }
+    }
+    printf("\n");
+    return 0;
+}
