4e831be955fe6d67a28f21f68e3efbea7d4a2461
Walescko <walescko@gmail.com>
2022-06-23 22:40:38 -0300

    c - class 3 array and vector

--- Mod01/Tema06/arrays/AverageClass3.c
+++ Mod01/Tema06/arrays/AverageClass3.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 //
 // Created by walescko on 21/06/22.
 //
@@ -9,31 +10,43 @@
 //A média de cada aluno
 //A média da turma
 
-int main(void){
+int main(void) {
 
     int maxAssessment = 11, students = 50, assessment = 3;
-    double assessment01[students], assessment02[students], assessment03[students], averegeStudent[students];
-    double sumAssessment01 = 0, sumAssessment02 = 0, sumAssessment03 =0;
+    double assessments[students][assessment], averegeStudent[students];
+    double sumAssessment01 = 0, sumAssessment02 = 0, sumAssessment03 = 0, sumAssessments = 0;
     double averegeAssessment01, averegeAssessment02, averegeAssessment03, averegeClass;
+    srand((unsigned)time(NULL));
 
     printf("i P1 P2 P3 Media\n");
-    for(int i = 0; i<students;i++) {
-        assessment01[i] = rand() % maxAssessment;
-        assessment02[i] = rand() % maxAssessment;
-        assessment03[i] = rand() % maxAssessment;
+    for (int i = 0; i < students; i++) {
+        printf("\n%d ", i + 1);
+        for (int j = 0; j < assessment; j++) {
+            assessments[i][j] = rand() % maxAssessment;
+            sumAssessments += assessments[i][j];
+            printf("%.2lf ", assessments[i][j]);
+            switch (j) {
+                case 0:
+                    sumAssessment01 += assessments[i][j];
+                    break;
+                case 1:
+                    sumAssessment02 += assessments[i][j];
+                    break;
+                case 2:
+                    sumAssessment03 += assessments[i][j];
+                    break;
+            }
 
-        sumAssessment01 += assessment01[i];
-        sumAssessment02 += assessment02[i];
-        sumAssessment03 += assessment03[i];
-        averegeStudent[i] = (assessment01[i]+assessment02[i]+assessment03[i])/3;
-
-        printf("%d %.2lf %.2lf %.2lf %.2lf\n", i+1, assessment01[i], assessment02[i], assessment03[i], averegeStudent[i]);
+        }
+        averegeStudent[i] = sumAssessments/assessment;
+        printf("%.2lf ", averegeStudent[i]);
+        sumAssessments = 0;
     }
 
+    averegeClass = (sumAssessment01+sumAssessment02+sumAssessment03)/(3*students);
     averegeAssessment01 = sumAssessment01/students;
     averegeAssessment02 = sumAssessment02/students;
     averegeAssessment03 = sumAssessment03/students;
-    averegeClass = (sumAssessment01 + sumAssessment02 + sumAssessment03)/(3*students);
 
     printf("\nDados da turma:");
     printf("\nMédia da turma %.2lf", averegeClass);
@@ -41,14 +54,7 @@ int main(void){
     printf("\nMédia P2: %.2lf", averegeAssessment02);
     printf("\nMédia P3: %.2lf\n", averegeAssessment03);
 
-
-
-
-
-
-
-
-
+    printf("\n");
     return 0;
 }
 
